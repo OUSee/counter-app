eeb91b100df5510ef11c842cf45dd426e14326ff
OUSee <newerworrying@gmail.com>
2024-07-11 19:31:04 +0300

    sfgsgesg

--- src/Components/Icons/MinusIcon.tsx
+++ src/Components/Icons/MinusIcon.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.module.css"
+
 interface IMinusIcon{
     width?: string;
     height?: string;
@@ -5,8 +7,8 @@ interface IMinusIcon{
 
 export const MinusIcon = ({ width = `18`, height = `18`}: IMinusIcon) => {
     return (
-        <svg width={width} height={height} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M9 18C13.9706 18 18 13.9706 18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0 13.9706 4.02944 18 9 18ZM4 10H14V8H4V10Z" fill="#343d3f"/>
+        <svg className={styles.svg}  width={width} height={height} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  fill-rule="evenodd" clip-rule="evenodd" d="M9 18C13.9706 18 18 13.9706 18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0 13.9706 4.02944 18 9 18ZM4 10H14V8H4V10Z" fill="#343d3f"/>
         </svg>
     )
 }
--- src/Components/Icons/PlusIcon.tsx
+++ src/Components/Icons/PlusIcon.tsx
@@ -1,3 +1,6 @@
+import styles from "./styles.module.css"
+
+
 interface IPlusIcon{
     width?: string;
     height?: string;
@@ -5,7 +8,7 @@ interface IPlusIcon{
 
 export const PlusIcon = ({ width = `18`, height = `18`}: IPlusIcon) => {
     return (
-        <svg width={width} height={height} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={styles.svg} width={width} height={height} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M18 9C18 13.9706 13.9706 18 9 18C4.02944 18 0 13.9706 0 9C0 4.02944 4.02944 0 9 0C13.9706 0 18 4.02944 18 9ZM10 10V14H8V10H4V8H8V4H10V8H14V10H10Z" fill="#343d3f"/>
         </svg>
     )
